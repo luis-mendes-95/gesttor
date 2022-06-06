7f265825ea1f81cf387ac4963f54c31769bdf26f
luis-mendes-95 <luis.mendes.95@hotmail.com>
2022-06-05 21:40:37 -0300

    Created some items

--- modulos/login/conecta_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* a $conecta_db é uma variável que recebe como atributo o ato de conectar ao banco de dados LOCALHOST
-usando o usuário ROOT e a senha VAZIA e por último acessar ao database gesttor */
-
-$conecta_db = mysqli_connect('localhost','root','','gesttor');
-
-?>
