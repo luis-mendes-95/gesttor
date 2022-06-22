83b03f742e2d33d5c80ecf4d445a590b7406c1f9
luis-mendes-95 <luis.mendes.95@hotmail.com>
2022-06-22 10:28:02 -0300

    added script

---
+++ src/script/script.js
@@ -0,0 +1,13 @@
+function testaPosicao(lista, posicao)
+  {
+    if (carrinhoDeCompras.length == 'Vou ser Dev') {
+        console.log('o item está no carrinho')
+    }
+  }
+  let carrinhoDeCompras =
+  [
+   "Funko Pop Kakashi",
+   "Dragon Ball Collection",
+   "Vou ser Dev"
+  ]
+  testaPosicao(carrinhoDeCompras, 0)
